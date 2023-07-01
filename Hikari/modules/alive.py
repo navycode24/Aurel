6fa6f43efc580ca366f0735e24a158a3fdb52c02
Robotikaazazel <73426077+Robotikaazazel@users.noreply.github.com>
2023-07-01 13:44:56 +0700

    update alive.py

--- Hikari/modules/alive.py
+++ Hikari/modules/alive.py
@@ -13,12 +13,12 @@ PHOTO = "https://telegra.ph//file/591dc2274c6fd6486b35b.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
-  TEXT = f"**ʜɪ [{event.sender.first_name}](tg://user?id={event.sender.id}), ᴀᴋᴜ ᴋᴀᴢᴜ ꭙ ꝛᴏʙᴏᴛ.** \n\n"
+  TEXT = f"**ʜɪ [{event.sender.first_name}](tg://user?id={event.sender.id}), Aᴋᴜ ᴋɪɴɢ ᴇx ʀᴏʙᴏᴛ.** \n\n"
   TEXT += "๏ **ᴀᴋᴜ sᴇʟᴀʟᴜ ʜɪᴅᴜᴘ ᴅᴀɴ ʙᴇᴋᴇʀᴊᴀ** \n\n"
-  TEXT += f"๏ **ᴍʏ ᴏᴡɴᴇʀ : [κ λ z υ](https://t.me/kenapatagkazu)** \n\n"
+  TEXT += f"๏ **ᴍʏ ᴏᴡɴᴇʀ : [𝔼𝕩](https://t.me/Masex77)** \n\n"
   TEXT += f"๏ **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{telever}` \n\n"
   TEXT += f"๏ **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{tlhver}` \n\n"
   TEXT += f"๏ **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pyrover}` \n\n"
   TEXT += "**ᴛᴇʀɪᴍᴀᴋᴀsɪʜ sᴜᴅᴀʜ ᴍᴇɴᴀᴍʙᴀʜᴋᴀɴ ᴋᴜ ᴅɪsɪɴɪ 🔥**"
-  BUTTON = [[Button.url("ʜᴇʟᴘ​", "https://t.me/Dark_musikbot?start=help"), Button.url("ᴅᴏɴᴀsɪ ​", "https://t.me/kenapatagkazu")]]
+  BUTTON = [[Button.url("ʜᴇʟᴘ​", "https://t.me/Kingexrobot?start=help"), Button.url("ᴅᴏɴᴀsɪ ​", "https://t.me/Masex77")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
