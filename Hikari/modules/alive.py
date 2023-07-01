468be287c8fb613492e86c28ea5ccda76d38f904
Robotikaazazel <73426077+Robotikaazazel@users.noreply.github.com>
2023-07-01 17:14:52 +0700

    update alive.py

--- Hikari/modules/alive.py
+++ Hikari/modules/alive.py
@@ -9,16 +9,16 @@ from Hikari.events import register
 from Hikari import telethn as tbot
 
 
-PHOTO = "https://telegra.ph//file/d59e9e84580a9cc6a3411.jpg"
+PHOTO = "https://telegra.ph//file/ede719f75f744df950219.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
-  TEXT = f"**ʜɪ [{event.sender.first_name}](tg://user?id={event.sender.id}), Aᴋᴜ ᴋɪɴɢ ᴇx ʀᴏʙᴏᴛ.** \n\n"
+  TEXT = f"**ʜɪ [{event.sender.first_name}](tg://user?id={event.sender.id}), Aᴋᴜ ᴀᴜʀᴇʟ ᴍᴜsɪᴄ ʙᴏᴛ.** \n\n"
   TEXT += "๏ **ᴀᴋᴜ sᴇʟᴀʟᴜ ʜɪᴅᴜᴘ ᴅᴀɴ ʙᴇᴋᴇʀᴊᴀ** \n\n"
   TEXT += f"๏ **ᴍʏ ᴏᴡɴᴇʀ : [𝔼𝕩](https://t.me/Masex77)** \n\n"
   TEXT += f"๏ **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{telever}` \n\n"
   TEXT += f"๏ **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{tlhver}` \n\n"
   TEXT += f"๏ **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pyrover}` \n\n"
   TEXT += "**ᴛᴇʀɪᴍᴀᴋᴀsɪʜ sᴜᴅᴀʜ ᴍᴇɴᴀᴍʙᴀʜᴋᴀɴ ᴋᴜ ᴅɪsɪɴɪ 🔥**"
-  BUTTON = [[Button.url("ʜᴇʟᴘ​", "https://t.me/Kingexrobot?start=help"), Button.url("ᴅᴏɴᴀsɪ ​", "https://t.me/Masex77")]]
+  BUTTON = [[Button.url("ʜᴇʟᴘ​", "https://t.me/Dark_musikbot?start=help"), Button.url("ᴅᴏɴᴀsɪ ​", "https://t.me/cdkagenouu")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
