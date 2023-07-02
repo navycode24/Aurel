c7dd0c12561917f5da03ca26e58a2ee65534e916
Robotikaazazel <73426077+Robotikaazazel@users.noreply.github.com>
2023-07-02 14:02:09 +0700

    update __init__.py

--- Hikari/__init__.py
+++ Hikari/__init__.py
@@ -38,9 +38,9 @@ logging.getLogger("pyrogram").setLevel(logging.INFO)
 logging.getLogger('ptbcontrib.postgres_persistence.postgrespersistence').setLevel(logging.WARNING)
 
 LOGGER = logging.getLogger('[Hikari]')
-LOGGER.info("King Ex is starting. | An King Ex Robot Parts. | Licensed under GPLv3.")
+LOGGER.info("Joox Music is starting. | An Joox Music Parts. | Licensed under GPLv3.")
 LOGGER.info("Not affiliated to other anime or Villain in any way whatsoever.")
-LOGGER.info("Project maintained by: github.com/Robotikaazazel (t.me/Masex77)")
+LOGGER.info("Project maintained by: github.com/Robotikaazazel (t.me/cdkagenouu)")
 
 # if version < 3.9, stop bot.
 if sys.version_info[0] < 3 or sys.version_info[1] < 8:
